00d8e7194de64acfad3c373d6e67cea7ba1d2609
Stasyambra <kbars87@mail.ru>
2016-04-02 23:04:25 +0300

    new versions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,14 @@
 package com.company;
 
 import javax.sql.rowset.serial.SerialRef;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World!!!");
+        System.out.println(new Date());
+        System.getProperties().list(System.out);
     }
 }
